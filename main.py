4a6b483900d991974f47c3720227e66ad861241d
Samppa-code <60503282+Samppa-code@users.noreply.github.com>
2021-02-17 15:17:53 +0200

    Lisätty main ja sanity

---
+++ main.py
@@ -0,0 +1,11 @@
+# Tämä on painonhallintasovelluksen pääohjelma
+
+# Kirjastojen ja moduulien käyttöönotot
+import sanity as s
+# Varsinaisen pääohjelman alku
+
+# Kirjastojen alustukset
+
+# Työsilmukka
+
+s.on_jarkeva()
